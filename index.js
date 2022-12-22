fb135be3d364c213f6a45ad8d6dacf2b81695598
Murugan Athmagnanam <1murugan23@gmail.com>
2022-12-22 09:00:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,63 @@ app.get('/', (req, res) => {
   res.send('Response initiated from home page');
 });
 
+app.get('/getAllEmployees', (req, res) => {
+  res.end(employeeDetails);
+});
+
+app.get('/getAllEmployeeById:id', (req, res) => {
+  var input = req.params.id;
+  var employeeRes = employeeDetails.filter((employee) => input == employee.id);
+  res.end(employeeRes);
+});
+
+app.get('/getAllEmployeeByName:name', (req, res) => {
+  var inputName = req.params.name;
+  var employeeName = employeeDetails.filter(
+    (employee) => inputName == employee.name
+  );
+  res.end(employeeName);
+});
+
+app.use(express.json());
+
+app.post('/insertEmployeeData', (req, res) => {
+  var newObj = {
+    id: req.body.id,
+    name: req.body.name,
+    dept: req.body.dept,
+    desig: req.body.desig,
+  };
+
+  employeeDetails.push(newObj);
+  var formData = `The values inserted succesfully - ${id}_${name}_${dept}_${desig}`;
+
+  res.end(formData);
+});
+
+app.put('/updateEmployeeData', (req, res) => {
+  var inputId = req.param.id;
+  var updateData = {
+    id: req.body.id,
+    name: req.body.name,
+    dept: req.body.dept,
+    desig: req.body.desig,
+  };
+
+  var getIndex = employeeDetails.findIndex((ele) => inputId == ele.id);
+  employeeDetails[getIndex] = updateData;
+
+  res.send(employeeDetails);
+});
+
+app.delete('/deleteRecord', (req, res) => {
+  var deleteID = req.query.id;
+  var getIndex = employeeDetails.findIndex((ele) => deleteID == ele.id);
+  if (!getIndex) res.end('The request is invalid');
+  employeeDetails.splice(getIndex, 1);
+  res.end(employeeDetails);
+});
+
 //Retrieve data through Query string in URL
 app.get('/login', (req, res) => {
   var username = req.query.name;
@@ -49,7 +106,7 @@ app.post('/submitData', (req, res) => {
   var name = req.body.name;
   var salary = req.body.salary;
 
-  if (name != '') res.end('The request is valid');
+  if (name == '') res.end('The request is in valid');
   var formData = `${id}_${name}_${salary}`;
 
   res.end(formData);
